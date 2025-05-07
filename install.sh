897e71c49233d3477cf6894a8a8ce642e9d9e589
Jesse Kane <jessekane66@gmail.com>
2025-05-07 18:46:09 -0400

    new install script

--- install.sh
+++ install.sh
@@ -1,20 +1,43 @@
-ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-SERVICE_NAME=samuraisight
-LAUNCH_PATH="$ROOT_DIR/bin/$SERVICE_NAME"
-SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME.service"
+REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ROOT_DIR="/opt/SamuraiSight"
+SERVICE_NAME=smsight
+SERVICE_FILE="/etc/systemd/system/smsight.service"
 echo -e "------------- SamuraiSight Installer -------------\n"
 
+echo "Installing SamuraiSight in $ROOT_DIR"
+
 # Check if samuraisight is already installed
-if [ -f $SERVICE_FILE ]; then
+if [ -d $ROOT_DIR ]; then
     echo "SamuraiSight is already installed"
     echo "Aborting installation"
     exit 1 # 1 denotes that samuraisight is already installed
 fi
 
-USER=samuraisight
-useradd --system --no-create-home --shell /usr/sbin/nologin $USER
+# Setting up the deployment directory
+mkdir $ROOT_DIR
+mkdir $ROOT_DIR/src
+mkdir $ROOT_DIR/resources
+mkdir $ROOT_DIR/bin
+# Copying essential code from the repo to the deployment directory
+cp -r $REPO_DIR/src $ROOT_DIR/src
+cp -r $REPO_DIR/resources $ROOT_DIR/resources
+cp -r $REPO_DIR/bin $ROOT_DIR/bin
+cp $REPO_DIR/uninstall.sh $ROOT_DIR/uninstall.sh
+cp $REPO_DIR/requirements.txt $ROOT_DIR/requirements.txt
+cp $REPO_DIR/config.toml $ROOT_DIR/config.toml
+
+# Loading service file
+cp $REPO_DIR/smsight.service $SERVICE_FILE
+systemctl daemon-reload
+
+# Changing working directory to deployment directory in order to complete installation
+cd $ROOT_DIR
+
+useradd --system --no-create-home --shell /usr/sbin/nologin smsight-srv
 usermod -aG video samuraisight
+
 read -p "Enter team number: " TEAM_NUMBER
+read -p "Enter device name: " DEV_NAME
 
 # Create .env file
 ENV_FILE=".env"
@@ -22,9 +45,9 @@ cat > $ENV_FILE <<EOF
 # Environment Configuration (Only edit this file if you know what you are doing)
 APP_ENV=production
 DEBUG=false
-ROOT_DIR=$ROOT_DIR
-USER=$USER
 TEAM=$TEAM_NUMBER
+SERVICE_FILE=$SERVICE_FILE
+DEV_NAME=$DEV_NAME
 EOF
 echo ".env file created at $(realpath "$ENV_FILE")"
 
@@ -44,44 +67,19 @@ echo "GATEWAY=$GATEWAY" >> $ENV_FILE
 ROBORIO_IP="10.$TE_AM.2"
 echo "ROBORIO_IP=$ROBORIO_IP" >> $ENV_FILE
 
-# Make samuraisight (the launch script) an executable and add it to the PATH variable
-chmod +x $LAUNCH_PATH
-
-echo "LAUNCH_ON_STARTUP=true" >> $ENV_FILE
-cat <<EOF > /tmp/temp_service
-[Unit]
-Description=Robot Vision System
-After=network.target
-
-[Service]
-ExecStart=$LAUNCH_PATH
-Restart=on-failure
-WorkingDirectory=$ROOT_DIR
-User=$USER
-AmbientCapabilities=CAP_NET_ADMIN
-CapabilityBoundingSet=CAP_NET_ADMIN
-NoNewPrivileges=true
-
-[Install]
-WantedBy=multi-user.target
-EOF
-mv /tmp/temp_service $SERVICE_FILE
-systemctl daemon-reload
-echo "SERVICE_FILE=$SERVICE_FILE" >> $ENV_FILE
-echo "Configured Systemd service"
-
+# Make samuraisight (the launch script) an executable
+chmod +x bin/smsight
 
 read -p "Do you want SamuraiSight to launch on startup? [Y/N]: " launchOnStartup
 if [ "$launchOnStartup" == "Y" ]; then
-    echo "LAUNCH_ON_STARTUP=true" >> $ENV_FILE
-    systemctl enable "$SERVICE_NAME.service"
-    echo "Systemd service successfully enabled."
-    echo "SamuraiSight will now start automatically on boot"
+    systemctl enable "smsight.service"
+    echo "SamuraiSight will launch automatically on startup. This can be disabled with sudo systemctl disable smsight.service"
 elif [ "$launchOnStartup" == "N" ]; then
-    echo "LAUNCH_ON_STARTUP=false" >> $ENV_FILE
+    echo "SamuraiSight will not launch on startup. To enable launch on startup, run sudo systemctl enable smsight.service"
 else
     echo "ERROR: Unrecognized response \"$launchOnStartup\""
-    exit 2 #2 denotes that user input was malformed
+    echo "Defaulting to launch on startup. This can be disabled by running sudo systemctl disable smsight.service"
+    systemctl enable "smsight.service"
 fi
 
 read -p "Do you want to set a static IP Address? [Y/N]: " SetStatic
@@ -91,13 +89,15 @@ if [ "$SetStatic" == "Y" ]; then
     echo "STATIC_IP=$StaticIP" >> $ENV_FILE
 elif [ "$SetStatic" == "N" ]; then
     echo "USE_STATIC_IP=false" >> $ENV_FILE
+    echo "Using DHCP. WARNING: It is HIGHLY recommended to set a static IP for competitions"
+    echo "To enable Static IP, set USE_STATIC_IP to true in the .env file, and add an entry called STATIC_IP assigned to the ip address"
 else
     echo "ERROR: Unrecognized response \"$SetStatic\""
-    exit 2 #2 denotes that user input was malformed
+    echo "Defaulting to DHCP"
+    echo "To enable Static IP, set USE_STATIC_IP to true in the .env file, and add an entry called STATIC_IP assigned to the ip address"
+    echo "USE_STATIC_IP=false" >> $ENV_FILE
 fi
 
-# This ensures the working directory is the root directory
-cd $ROOT_DIR
 VENV=".venv"
 python3 -m venv $VENV
 source ".venv/bin/activate"
@@ -106,6 +106,4 @@ echo "Successfully created python virtual environment"
 # Creates logs/ directory, models/ directory, and recordings/ directory
 mkdir logs
 mkdir models
-mkdir recordings
-
 exit 0 #0 means successful installation
--- uninstall.sh
+++ uninstall.sh
@@ -1,15 +1,15 @@
 echo "Uninstalling SamuraiSight"
 
-SERVICE_NAME="samuraisight.service"
-SERVICE_FILE="/etc/systemd/system/samuraisight.service"
+SERVICE_NAME="smsight.service"
+SERVICE_FILE="/etc/systemd/system/smsight.service"
+ROOT_DIR="/opt/SamuraiSight"
 
-if [ ! -f $SERVICE_FILE ]; then
+if [ ! -d $ROOT_DIR ]; then
     echo "SamuraiSight is not installed"
     exit 0
 fi
 
-ROOT_DIR=$(grep -E '^WorkingDirectory=' $SERVICE_FILE | cut -d= -f2)
-USER=$(grep -E '^User=' $SERVICE_FILE | cut -d= -f2)
+USER="smsight-srv"
 systemctl stop $SERVICE_NAME
 systemctl disable $SERVICE_NAME
 userdel $USER
