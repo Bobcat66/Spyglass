3ef938bea3e274ef90f2dc0d910c8860595dc719
Jesse Kane <jessekane66@gmail.com>
2025-05-07 14:28:02 -0400

    uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -2,9 +2,21 @@
 
 echo "Uninstalling SamuraiSight"
 
-if [ ! -f "/etc/systemd/system/samuraisight.service" ]; then
+SERVICE_NAME="samuraisight.service"
+SERVICE_FILE="/etc/systemd/system/samuraisight.service"
+
+if [ ! -f $SERVICE_FILE ]; then
     echo "SamuraiSight is not installed"
     exit 0
 fi
+
+ROOT_DIR=$(grep -E '^WorkingDirectory=' $SERVICE_FILE | cut -d= -f2)
+sudo systemctl stop $SERVICE_NAME
+sudo systemctl disable $SERVICE_NAME
+sudo rm $SERVICE_FILE
+sudo rm -r ROOT_DIR
+
+
+
 
 
