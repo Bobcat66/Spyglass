3154db0712b485c0362157ccf2dd2714013fc3c0
Jesse Kane <jessekane66@gmail.com>
2025-05-07 14:28:58 -0400

    im stupid

--- uninstall.sh
+++ uninstall.sh
@@ -14,7 +14,7 @@ ROOT_DIR=$(grep -E '^WorkingDirectory=' $SERVICE_FILE | cut -d= -f2)
 sudo systemctl stop $SERVICE_NAME
 sudo systemctl disable $SERVICE_NAME
 sudo rm $SERVICE_FILE
-sudo rm -r ROOT_DIR
+sudo rm -r $ROOT_DIR
 
 
 
