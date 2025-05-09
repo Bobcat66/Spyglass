70c0d159c158a61b9f2992685cb4cc3152a534ef
Jesse Kane <jessekane66@gmail.com>
2025-05-09 12:50:38 -0400

    h

--- install.sh
+++ install.sh
@@ -145,7 +145,7 @@ cp $REPO_DIR/config.toml $ROOT_DIR/config.toml
 cp $REPO_DIR/services/smsight.service $SERVICE_FILE
 cp $REPO_DIR/services/smsight-rootsrv.service $ROOTSRV_FILE
 systemctl daemon-reload
-systemctl enable rootsrv.service
+systemctl enable smsight-rootsrv.service
 
 # Changing working directory to deployment directory in order to complete installation
 cd $ROOT_DIR
