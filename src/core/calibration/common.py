9b2b10435924ebc58f8223e357f6c1d435c8a525
Jesse Kane <jessekane66@gmail.com>
2025-05-13 12:36:51 -0400

    worked on camera calibration backend

--- src/core/calibration/common.py
+++ src/core/calibration/common.py
@@ -1,6 +1,4 @@
 from abc import ABC, abstractmethod
-import cv2
-import cv2.aruco as aruco
 from dataclasses import dataclass
 import numpy as np
 from typing import Tuple, List
@@ -21,6 +19,9 @@ class CalibrationInput:
     size: Tuple[int,int]
     fx_seed: float
     fy_seed: float
+    cx_seed: float
+    cy_seed: float
+    dist_coeffs_seed: np.typing.NDArray[np.float64]
 
 @dataclass
 class CalibrationData:
@@ -35,9 +36,12 @@ class CalibrationData:
     def getNumpyDistCoeffs(self) -> np.typing.NDArray[np.float64]:
         return np.array(self.dist_coeffs,dtype=np.float64)
 
-class CharucoDetector():
-    def __init__(self,dict: aruco.Dictionary,params: aruco.DetectorParameters,board: aruco.CharucoBoard):
-        pass
+class CalibrationModule(ABC):
+
+    @abstractmethod
+    def calibrate(self, input: CalibrationInput) -> CalibrationData:
+        raise NotImplementedError()
+
         
 
 
---
+++ src/core/calibration/impl/OpenCVImpl.py
@@ -0,0 +1,32 @@
+from calibration.common import *
+import cv2
+
+class OpenCVImpl(CalibrationModule):
+    def calibrate(self, input: CalibrationInput) -> CalibrationData:
+        objectPoints = []
+        imgPoints = []
+        for obs in input.observations:
+            objectPoints.append(obs.object_points)
+            imgPoints.append(obs.img_points)
+        camera_matrix = np.array([
+            [input.fx_seed,0.00000000000,input.cx_seed],
+            [0.00000000000,input.fy_seed,input.cy_seed],
+            [0.00000000000,0.00000000000,1.00000000000]
+        ])
+        dist_coefs = input.dist_coeffs_seed.copy()
+        rms, camera_matrix, dist_coefs, rvecs, tvecs = cv2.calibrateCamera(
+            objectPoints,
+            imgPoints,
+            input.size,
+            camera_matrix,
+            dist_coefs,
+            None,
+            None,
+            cv2.CALIB_USE_INTRINSIC_GUESS
+        )
+        return CalibrationData(
+            input.size,
+            camera_matrix.tolist(),
+            dist_coefs.tolist(),
+            rms
+        )
